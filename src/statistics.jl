a4ab2702223f9298fa05663bc1a8da3bbbf8610f
Benoit Richard <brichard@desy.de>
2026-04-16 11:22:10 +0200

    Allow 2sigma error in banderror

--- src/statistics.jl
+++ src/statistics.jl
@@ -5,11 +5,35 @@ Plot the data `(xx, yy)` as a line, with the error `err` represented by a band.
 
 The error is of the form `yy ± err`, so that the width of the band is `2 * err`.
 """
-function banderror!(ax, xx, yy, err ; band_alpha = 0.5, color = :gray, band_kwargs = (;), kwargs...)
+function banderror!(ax, xx, yy, err ;
+        band_alpha = 0.7,
+        color = palette[1],
+        band_kwargs = (;),
+        double_error = false, 
+        error_label = "1σ error",
+        kwargs...)
+
+    if double_error
+        band!(ax, xx, yy - 2err, yy - err ;
+            alpha = band_alpha / 2,
+            color,
+            label = "2σ error",
+            band_kwargs...
+        )
+
+        band!(ax, xx, yy + err, yy + 2err ;
+            alpha = band_alpha / 2,
+            color,
+            label = "2σ error",
+            band_kwargs...
+        )
+    end
+
     band!(ax, xx, yy - err, yy + err ;
         alpha = band_alpha,
         color,
+        label = error_label,
         band_kwargs...
     )
-    lines!(ax, xx, yy ; color, kwargs...)
+    lines!(ax, xx, yy ; color = :black, label = "Average", kwargs...)
 end
