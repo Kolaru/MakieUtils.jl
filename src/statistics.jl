1d9af4507f46ad5c50458d37675187773eec87b6
Benoit Richard <brichard@desy.de>
2026-06-15 15:29:40 +0200

    More control ove banderror

--- src/statistics.jl
+++ src/statistics.jl
@@ -9,23 +9,24 @@ function banderror!(ax, xx, yy, err ;
         band_alpha = 0.7,
         color = palette[1],
         band_kwargs = (;),
+        line_kwargs = (;),
+        scatter_kwargs = (;),
         double_error = false, 
-        error_label = "1σ error",
         marker = nothing,
-        kwargs...)
+        label = "Average")
 
     if double_error
         band!(ax, xx, yy - 2err, yy - err ;
             alpha = band_alpha / 2,
             color,
-            label = "2σ error",
+            label,
             band_kwargs...
         )
 
         band!(ax, xx, yy + err, yy + 2err ;
             alpha = band_alpha / 2,
             color,
-            label = "2σ error",
+            label,
             band_kwargs...
         )
     end
@@ -33,12 +34,12 @@ function banderror!(ax, xx, yy, err ;
     band!(ax, xx, yy - err, yy + err ;
         alpha = band_alpha,
         color,
-        label = error_label,
+        label,
         band_kwargs...
     )
-    lines!(ax, xx, yy ; color = :black, label = "Average", kwargs...)
+    lines!(ax, xx, yy ; color = :black, label, line_kwargs...)
 
     if !isnothing(marker)
-        scatter!(ax, xx, yy ; color = :black, label = "Average", marker, kwargs...)
+        scatter!(ax, xx, yy ; color = :black, label, marker, scatter_kwargs...)
     end
 end
